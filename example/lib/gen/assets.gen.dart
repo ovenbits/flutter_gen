b6f574cd023e280cc8c481dc8254c599c6552430
Igor K <igor.k@letsmake.com>
2022-07-14 00:49:44 +0300

    return back image assets generation, fix example compilation errors

--- example/lib/gen/assets.gen.dart
+++ example/lib/gen/assets.gen.dart
@@ -7,9 +7,9 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import
 
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flare_flutter/flare_controller.dart';
 import 'package:rive/rive.dart';
@@ -18,7 +18,7 @@ class $PicturesGen {
   const $PicturesGen();
 
   /// File path: pictures/chip5.jpg
-  String get chip5 => 'pictures/chip5.jpg';
+  AssetGenImage get chip5 => const AssetGenImage('pictures/chip5.jpg');
 }
 
 class $AssetsFlareGen {
@@ -32,23 +32,23 @@ class $AssetsImagesGen {
   const $AssetsImagesGen();
 
   /// File path: assets/images/chip1.jpg
-  String get chip1 => 'assets/images/chip1.jpg';
+  AssetGenImage get chip1 => const AssetGenImage('assets/images/chip1.jpg');
 
   /// File path: assets/images/chip2.jpg
-  String get chip2 => 'assets/images/chip2.jpg';
+  AssetGenImage get chip2 => const AssetGenImage('assets/images/chip2.jpg');
 
   $AssetsImagesChip3Gen get chip3 => const $AssetsImagesChip3Gen();
   $AssetsImagesChip4Gen get chip4 => const $AssetsImagesChip4Gen();
   $AssetsImagesIconsGen get icons => const $AssetsImagesIconsGen();
 
   /// File path: assets/images/logo.png
-  String get logo => 'assets/images/logo.png';
+  AssetGenImage get logo => const AssetGenImage('assets/images/logo.png');
 
   /// File path: assets/images/profile.jpg
-  String get profileJpg => 'assets/images/profile.jpg';
+  AssetGenImage get profileJpg => const AssetGenImage('assets/images/profile.jpg');
 
   /// File path: assets/images/profile.png
-  String get profilePng => 'assets/images/profile.png';
+  AssetGenImage get profilePng => const AssetGenImage('assets/images/profile.png');
 }
 
 class $AssetsJsonGen {
@@ -89,14 +89,14 @@ class $AssetsImagesChip3Gen {
   const $AssetsImagesChip3Gen();
 
   /// File path: assets/images/chip3/chip3.jpg
-  String get chip3 => 'assets/images/chip3/chip3.jpg';
+  AssetGenImage get chip3 => const AssetGenImage('assets/images/chip3/chip3.jpg');
 }
 
 class $AssetsImagesChip4Gen {
   const $AssetsImagesChip4Gen();
 
   /// File path: assets/images/chip4/chip4.jpg
-  String get chip4 => 'assets/images/chip4/chip4.jpg';
+  AssetGenImage get chip4 => const AssetGenImage('assets/images/chip4/chip4.jpg');
 }
 
 class $AssetsImagesIconsGen {
@@ -125,6 +125,69 @@ class Assets {
   static const $AssetsRiveGen rive = $AssetsRiveGen();
   static const $AssetsUnknownGen unknown = $AssetsUnknownGen();
   static const $PicturesGen pictures = $PicturesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
 }
 
 class SvgGenImage {
